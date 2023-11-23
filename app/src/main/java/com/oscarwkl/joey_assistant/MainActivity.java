3aed6612bebce474a36e667b8490f8db5ff74be4
yodaplaysdroid <oscarwongkinnlap@gmail.com>
2023-11-23 14:19:25 +0800

    main activity layout created

---
+++ app/src/main/java/com/oscarwkl/joey_assistant/MainActivity.java
@@ -0,0 +1,18 @@
+package com.oscarwkl.joey_assistant;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Your code goes here
+    }
+}
